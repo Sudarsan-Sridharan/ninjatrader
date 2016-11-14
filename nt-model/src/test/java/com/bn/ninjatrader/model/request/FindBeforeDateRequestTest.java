5e54644160509d1d0b80fe3a4896996eeaebb605
Brad Wee <bradford.wee@practiceinsight.io>
2016-11-14 20:52:37 +0800

    Add tests Refactored method names

---
+++ nt-model/src/test/java/com/bn/ninjatrader/model/request/FindBeforeDateRequestTest.java
@@ -0,0 +1,94 @@
+package com.bn.ninjatrader.model.request;
+
+import com.beust.jcommander.internal.Sets;
+import com.bn.ninjatrader.common.data.Value;
+import com.bn.ninjatrader.common.type.TimeFrame;
+import org.testng.annotations.Test;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+import static org.testng.Assert.*;
+import static org.testng.AssertJUnit.assertTrue;
+
+/**
+ * Created by Brad on 7/27/16.
+ */
+public class FindBeforeDateRequestTest {
+
+  private LocalDate date = LocalDate.of(2016, 1, 1);
+  private Value value1 = Value.of(date, 1d);
+  private Value value2 = Value.of(date, 2d);
+
+  private SaveRequest orig = SaveRequest.save("MEG").period(1).values(value1);
+  private SaveRequest same = SaveRequest.save("MEG").period(1).values(value1);
+
+  private SaveRequest differentSymbol = SaveRequest.save("BDO").period(1).values(value1);
+  private SaveRequest differentPeriod = SaveRequest.save("MEG").period(2).values(value1);
+
+  private SaveRequest differentValue = SaveRequest.save("MEG").period(1).values(value2);
+  private SaveRequest differentValueSize = SaveRequest.save("MEG").period(1).values(value1, value1);
+
+  private SaveRequest differentTimeFrame = SaveRequest.save("MEG").period(1).values(value1).timeFrame(TimeFrame.ONE_WEEK);
+
+  @Test
+  public void testEqualsWithSameObject() {
+    assertTrue(orig.equals(orig));
+  }
+
+  @Test
+  public void testEqualsWithSameValue() {
+    assertTrue(orig.equals(same));
+    assertTrue(same.equals(orig));
+  }
+
+  @Test
+  public void testEqualsWithDifferences() {
+    assertFalse(orig.equals(differentSymbol));
+    assertFalse(orig.equals(differentPeriod));
+    assertFalse(orig.equals(differentValue));
+    assertFalse(orig.equals(differentValueSize));
+  }
+
+  @Test
+  public void testEqualsWithDiffObjectType() {
+    assertFalse(orig.equals(new Object()));
+    assertFalse(orig.equals("Wrong Object"));
+    assertFalse(orig.equals(1));
+    assertFalse(orig.equals(null));
+  }
+
+  @Test
+  public void testEqualsWIthDiffTimeFrame() {
+    assertFalse(orig.equals(differentTimeFrame));
+  }
+
+  @Test
+  public void testHashCode() {
+    assertNotEquals(orig.hashCode(), differentSymbol.hashCode());
+    assertNotEquals(orig.hashCode(), differentPeriod.hashCode());
+    assertNotEquals(orig.hashCode(), differentValue.hashCode());
+    assertNotEquals(orig.hashCode(), differentValueSize.hashCode());
+    assertNotEquals(orig.hashCode(), differentTimeFrame.hashCode());
+
+    assertEquals(orig.hashCode(), same.hashCode());
+  }
+
+  @Test
+  public void testHashCodeWithSet() {
+    Set<SaveRequest> set = Sets.newHashSet();
+
+    set.add(orig);
+    set.add(orig);
+    assertEquals(set.size(), 1); // Should overwrite
+
+    set.add(same);
+    assertEquals(set.size(), 1); // Should overwrite
+
+    set.add(differentSymbol);
+    assertEquals(set.size(), 2);
+
+    set.add(differentPeriod);
+    assertEquals(set.size(), 3);
+  }
+}
