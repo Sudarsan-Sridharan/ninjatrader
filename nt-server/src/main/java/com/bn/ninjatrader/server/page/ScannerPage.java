558a125a61b514119863f5b4937d6918680aa1ed
Brad Wee <bradford.wee@practiceinsight.io>
2017-02-25 19:41:10 +0800

    added scanner page

---
+++ nt-server/src/main/java/com/bn/ninjatrader/server/page/ScannerPage.java
@@ -0,0 +1,38 @@
+package com.bn.ninjatrader.server.page;
+
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.Velocity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Singleton;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+
+/**
+ * Created by Brad on 4/26/16.
+ */
+@Singleton
+@Path("/scanner")
+public class ScannerPage {
+  private static final Logger LOG = LoggerFactory.getLogger(ScannerPage.class);
+
+  @GET
+  @Produces(MediaType.TEXT_HTML)
+  public String showScanResults(@Context final HttpServletRequest req)
+      throws ServletException, IOException {
+    final Writer writer = new StringWriter();
+    final VelocityContext context = new VelocityContext();
+    context.put("contextPath", req.getContextPath());
+    Velocity.mergeTemplate("velocity/pages/scan.vm", "UTF-8", context, writer);
+    return writer.toString();
+  }
+}
---
+++ nt-simulation/src/test/java/com/bn/ninjatrader/simulation/report/SimulationReportTest.java
@@ -0,0 +1,7 @@
+package com.bn.ninjatrader.simulation.report;
+
+/**
+ * @author bradwee2000@gmail.com
+ */
+public class SimulationReportTest {
+}
